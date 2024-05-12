3af498e380d0242abf1f7f331d4775252bf2f9a0
Gonzalo Linares <golinares@itba.edu.ar>
2024-05-12 14:53:50 -0300

    updated

--- VGA/VGA_Driver.v
+++ VGA/VGA_Driver.v
@@ -5,16 +5,16 @@ module VGA_Driver(
 	input clk25MHz,         // 25 MHz
 	output hsync,      	// horizontal sync
 	output vsync,	      // vertical sync
-	output [3:0] red,
-	output [3:0] blue,
-	output [3:0] green  
+	output [2:0] red,
+	output [2:0] green,
+	output [1:0] blue	
 );
 
 	reg [9:0] counter_x = 0;  // horizontal counter
 	reg [9:0] counter_y = 0;  // vertical counter
-	reg [3:0] r_red = 0;
-	reg [3:0] r_blue = 0;
-	reg [3:0] r_green = 0;
+	reg [2:0] r_red = 0;
+	reg [2:0] r_green = 0;
+	reg [1:0] r_blue = 0;
 	
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,12 +38,10 @@ module VGA_Driver(
 				end 
 		end
 
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// hsync and vsync output assignments
 	assign hsync = (counter_x >= 0 && counter_x < 96) ? 1:0;  // hsync high for 96 counts                                                 
 	assign vsync = (counter_y >= 0 && counter_y < 2) ? 1:0;   // vsync high for 2 counts
 
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// pattern generate
 	always @ (posedge clk50MHz)
 	begin
@@ -54,8 +52,8 @@ module VGA_Driver(
 		
 	end
 						
-	assign red = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_red : 4'h0;
-	assign blue = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_blue : 4'h0;
-	assign green = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_green : 4'h0;
+	assign red = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_red : 2'b0;
+	assign blue = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_blue : 1'b0;
+	assign green = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_green : 2'b0;
 	
-endmodule  // VGA_image_gen
+endmodule
